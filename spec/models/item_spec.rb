f3d0d925871845e496b4bcfcb0de6d0b3ab760c5
your-name <your-email>
2014-03-13 19:59:26 -0600

    test for item class created within index.html.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,7 +6,16 @@ describe Item do
   it 'has a name' do
     item = Item.new("Channa Masala", 5.95, "Yummy goodness")
 
-    expect(item.name). eq "Channa Masala"
+    expect(item.name).to eq "Channa Masala"
   end
-end
+  it 'has a price' do
+    item = Item.new("Channa Masala", 5.95, "Yummy goodness")
 
+    expect(item.price).to eq 5.95
+  end
+  it 'has a description' do
+    item = Item.new("Channa Masala", 5.95, "Yummy goodness")
+
+    expect(item.description).to eq "Yummy goodness"
+  end
+  end
